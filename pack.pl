713597384598557a9469628bea45472326fc4c59
Nicos Angelopoulos <nicos@gmx.co.uk>
2013-01-10 18:38:30 +0100

    Added   pack.pl   only relevant to SWI (for now), but might as well have it here.

---
+++ pack.pl
@@ -0,0 +1,10 @@
+name(real).
+version('0.1.1').
+title('Integrative statistics with R').
+keywords([statistics,'R',bioinformatics,'machine learning']).
+author( 'Nicos Angelopoulos', 'http://bioinformatics.nki.nl/~nicos' ).
+author( 'Vitor Santos Costa', 'http:/www.dcc.fc.up.pt/~vsc' ).
+packager( 'Nicos Angelopoulos', 'http://bioinformatics.nki.nl/~nicos' ).
+maintainer( 'Nicos Angelopoulos', 'http://bioinformatics.nki.nl/~nicos' ).
+home( 'http://bioinformatics.nki.nl/~nicos/sware/real' ).
+download( 'http://bioinformatics.nki.nl/~nicos/sware/packs/real/real-*.tgz' ).
