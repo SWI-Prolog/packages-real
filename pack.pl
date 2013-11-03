4be6698a07abf0d2f8a1241d7e75364ba7715947
nicos angelopoulos <nicos@komos.(none)>
2013-11-03 08:32:50 +0000

    Version 0.1.2     (pack.pl).

--- pack.pl
+++ pack.pl
@@ -1,10 +1,10 @@
 name(real).
-version('0.1.1').
+version('0.1.2').
 title('Integrative statistics with R').
 keywords([statistics,'R',bioinformatics,'machine learning']).
-author( 'Nicos Angelopoulos', 'http://bioinformatics.nki.nl/~nicos' ).
+author( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
 author( 'Vitor Santos Costa', 'http:/www.dcc.fc.up.pt/~vsc' ).
-packager( 'Nicos Angelopoulos', 'http://bioinformatics.nki.nl/~nicos' ).
-maintainer( 'Nicos Angelopoulos', 'http://bioinformatics.nki.nl/~nicos' ).
-home( 'http://bioinformatics.nki.nl/~nicos/sware/real' ).
-download( 'http://bioinformatics.nki.nl/~nicos/sware/packs/real/real-*.tgz' ).
+packager( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
+maintainer( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
+home( 'http://stoics.org.uk/~nicos/sware/real' ).
+download( 'http://stoics.org.uk/~nicos/sware/packs/real/real-*.tgz' ).
