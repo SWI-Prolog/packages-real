13f92c14c8c2bf4ea99cf947ceeb95b637701a12
Nicos Angelopoulos <nicos@gmx.co.uk>
2013-11-22 13:59:48 +0000

    Added     example/for_real.pl ex(string) for SWI v7.

--- examples/for_real.pl
+++ examples/for_real.pl
@@ -379,7 +379,19 @@ ex(empty_args) :-
      <- plot( 1:10, 1:10 ),
      findall( I, (between(1,6,I),write('.'), flush_output, sleep(1)), _ ),
      nl,
-     devoff.
+     devoff.  % fixme use dev.off() when Yap starts supporting it.
+
+% ex(string).
+%
+%  Test new (2013/11/22) string type in SWI Prolog v7.
+%
+ex(string) :-
+	( (current_predicate(string/1),string("abc")) ->
+     	<- plot( 1:10, 1:10, main="native string type has arrived to Prolog" ),
+     	findall( I, (between(1,6,I),write('.'), flush_output, sleep(1)), _ )
+		;
+		true
+	).
 
 % ex(binary_op).
 %
